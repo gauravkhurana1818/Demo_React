de8dcf6f68328b66be24b984df3e3b68aac5fddc
Gaurav khurana <gauravkhurana@192.168.1.12>
2024-11-19 13:58:00 +0530

    first commit

--- src/assets/swag.js
+++ src/assets/swag.js
@@ -1,7 +1,7 @@
 export default [
   {
     id: "0001",
-    img: "https://images.bigcartel.com/product_images/196870528/201733_(3).jpg?auto=format&fit=max&h=1000&w=1000",
+    img: "https://static.wingify.com/gcp/wp-content/themes/vwo/images/new-website/newhome/home-2023/vwo-cutting-edge-tech@2x.png",
     price: 10.00.toFixed(2),
     title: "Snapback",
     description: "It's a hat. You wear it on your head.",
@@ -9,7 +9,7 @@ export default [
   },
   {
     id: "0002",
-    img: "https://images.bigcartel.com/product_images/192865621/devmtnshop_(12_of_25).jpg?auto=format&fit=max&h=1000&w=1000",
+    img: "https://static.wingify.com/gcp/wp-content/themes/vwo/images/new-website/newhome/home-2023/vwo-cutting-edge-tech@2x.png",
     price: 10.00.toFixed(2),
     title: "Beanie",
     description: "It's a beanie. You wear it on your head. 100% Acrylic with leather logo patch // One size fits all",
@@ -17,7 +17,7 @@ export default [
   },
   {
     id: "0003",
-    img: "https://images.bigcartel.com/product_images/192866269/devmtnshop_(21_of_25).jpg?auto=format&fit=max&h=1000&w=1000",
+    img: "https://static.wingify.com/gcp/wp-content/themes/vwo/images/new-website/newhome/home-2023/vwo-cutting-edge-tech@2x.png",
     price: 12.00.toFixed(2),
     title: "Lanyard",
     description: "Make life convenient with a lanyard. You add keys. It goes on your neck.",
@@ -25,7 +25,7 @@ export default [
   },
   {
     id: "0004",
-    img: "https://images.bigcartel.com/product_images/192865687/devmtnshop_(11_of_25).jpg?auto=format&fit=max&h=1000&w=1000",
+    img: "https://static.wingify.com/gcp/wp-content/themes/vwo/images/new-website/newhome/home-2023/vwo-cutting-edge-tech@2x.png",
     price: 30.00.toFixed(2),
     title: "Hoodie",
     description: "Independent Trading Company // (55% Cotton / 45% Polyester)",
